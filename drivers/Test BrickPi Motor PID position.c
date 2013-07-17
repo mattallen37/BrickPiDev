c8b8ed29dbd10a80a6ea7196a5929277d695bd4a
Matthew Richardson <matthewrichardson37@gmail.com>
2013-07-17 11:35:20 -0400

    Add motor position control test program.

---
+++ drivers/Test BrickPi Motor PID position.c
@@ -0,0 +1,65 @@
+/*
+*  Matthew Richardson
+*  matthewrichardson37<at>gmail.com
+*  http://mattallen37.wordpress.com/
+*  Initial date: June 4, 2013
+*  Last updated: July 17, 2013
+*
+*  You may use this code as you wish, provided you give credit where it's due.
+*
+*  This is a program for testing the RPi BrickPi drivers.
+*/
+
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+
+#include "tick.h"
+
+#include <wiringPi.h>
+
+#include "BrickPi.h"
+
+//#include <unistd.h>  
+//#include <errno.h>  
+//#include <stdio.h>  
+//#include <stdlib.h>  
+#include <linux/i2c-dev.h>  
+//#include <sys/ioctl.h>  
+#include <fcntl.h>
+
+// gcc -o program "Test BrickPi Motor PID position.c" -lrt -lm -L/usr/local/lib -lwiringPi
+// ./program
+
+int result;
+
+int main() {
+  ClearTick();
+
+  result = BrickPiSetup();
+  printf("BrickPiSetup: %d\n", result);
+  if(result)
+    return 0;
+
+  BrickPi.Address[0] = 1;
+  BrickPi.Address[1] = 2;
+  
+  BrickPi.Timeout = 100;                       // Communication timeout (how long in ms since the last valid communication before floating the motors). 0 disables the timeout.
+  if(BrickPiSetTimeout())
+    return 0;
+
+  BrickPi.MotorEnable[PORT_B] = TYPE_MOTOR_POSITION;
+  
+  result = BrickPiSetupSensors();
+  printf("BrickPiSetupSensors: %d\n", result); 
+  if(!result){    
+    while(1){
+      result = BrickPiUpdateValues();
+      if(!result){
+        BrickPi.MotorTarget[PORT_B] = BrickPi.Encoder[PORT_A];
+      }
+      usleep(10000);
+    }
+  }
+  return 0;
+}
